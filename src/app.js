b6add121d57410db8722fe4493bfda54bcb0c6ab
CodeBreaker <diegoperezperez518@gmail.com>
2023-03-06 23:57:29 -0600

    updated filter function

--- src/app.js
+++ src/app.js
@@ -48,30 +48,28 @@ const creaCards = () => {
   contenido.appendChild(fragment)
 }
 
-btnBuscar.addEventListener('keydown', (event) => {
-  if (event.keyCode === 13) { // 13 means Enter in ASCII.
-    let searchedSong = btnBuscar.value
-    console.log(searchedSong)
-    contenido.innerHTML = '' 
-    let filteredSongs = topTwoHundred.filter((song) => {
-      return song.trackMetadata.trackName.includes(searchedSong)
-    })
-    filteredSongs.forEach((song) => {
-        cardTop.querySelector('img').setAttribute('src', song.trackMetadata.displayImageUri)
-        cardTop.querySelector('.songname').textContent = song.trackMetadata.trackName
-        let artists = ''
-        let size = song.trackMetadata.artists.length
-        song.trackMetadata.artists.forEach((item, index) => {
-          if (index === size-1){
-            artists += item.name
-          } else{
-            artists += item.name + '/'
-          }
-        })
-        cardTop.querySelector('.artistname').textContent = artists
+btnBuscar.addEventListener('input', (event) => {
+  let searchedSong = event.target.value
+  console.log(searchedSong)
+  contenido.innerHTML = '' 
+  let filteredSongs = topTwoHundred.filter((song) => {
+    return song.trackMetadata.trackName.includes(searchedSong)
+  })
+  filteredSongs.forEach((song) => {
+      cardTop.querySelector('img').setAttribute('src', song.trackMetadata.displayImageUri)
+      cardTop.querySelector('.songname').textContent = song.trackMetadata.trackName
+      let artists = ''
+      let size = song.trackMetadata.artists.length
+      song.trackMetadata.artists.forEach((item, index) => {
+        if (index === size-1){
+          artists += item.name
+        } else{
+          artists += item.name + '/'
+        }
+      })
+      cardTop.querySelector('.artistname').textContent = artists
 
-        const clone = cardTop.cloneNode(true)
-        fragment.appendChild(clone)
-    })
-  }
+      const clone = cardTop.cloneNode(true)
+      fragment.appendChild(clone)
+  })
 })
