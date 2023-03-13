09c016413190e5eb4a6b4b2f9ee6e07c761f07cd
CodeBreaker <diegoperezperez518@gmail.com>
2023-03-13 16:14:29 -0600

    updated general & scroll styles

--- src/app.js
+++ src/app.js
@@ -1,77 +1,77 @@
-const cardTop = document.querySelector('#cardTop').content
-const contenido = document.querySelector('#contenido')
-const fragment = document.createDocumentFragment()
-const API = 'https://spotify81.p.rapidapi.com/top_200_tracks'
-const btnBuscar = document.querySelector('#buscador')
+const cardTop = document.querySelector('#cardTop').content;
+const contenido = document.querySelector('#contenido');
+const fragment = document.createDocumentFragment();
+const API = 'https://spotify81.p.rapidapi.com/top_200_tracks';
+const btnBuscar = document.querySelector('#buscador');
 let topTwoHundred = [];
 
 document.addEventListener('DOMContentLoaded', () => {
-  loadMusicData()
-})
+  loadMusicData();
+});
 
 const loadMusicData = () => {
   const options = {
     method: 'GET',
     headers: {
       'X-RapidAPI-Key': '747be6c6e0msh7e2183218542cbbp1e4682jsncb33cbcc42c3',
-      'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
-    }
-  }
+      'X-RapidAPI-Host': 'spotify81.p.rapidapi.com',
+    },
+  };
   fetch(API, options)
-    .then(response => response.json())
-    .then(response => {
-      topTwoHundred = response
-      console.log('Canciones', topTwoHundred)
-      creaCards()
+    .then((response) => response.json())
+    .then((response) => {
+      topTwoHundred = response;
+      console.log('Canciones', topTwoHundred);
+      creaCards();
     })
-    .catch(err => console.error(err))
-}
+    .catch((err) => console.error(err));
+};
 
 const creaCards = () => {
-    topTwoHundred.forEach((song) => {
-      cardTop.querySelector('img').setAttribute('src', song.trackMetadata.displayImageUri)
-      cardTop.querySelector('.songname').textContent = song.trackMetadata.trackName
-      let artists = ''
-      let size = song.trackMetadata.artists.length
-      song.trackMetadata.artists.forEach((item, index) => {
-        if (index === size-1){
-          artists += item.name
-        } else {
-          artists += item.name + '/'
-        }
-      })
-      cardTop.querySelector('.artistname').textContent = artists
+  topTwoHundred.forEach((song) => {
+    cardTop.querySelector('img').setAttribute('src', song.trackMetadata.displayImageUri);
+    cardTop.querySelector('.songname').textContent = song.trackMetadata.trackName;
+    let artists = '';
+    let size = song.trackMetadata.artists.length;
+    song.trackMetadata.artists.forEach((item, index) => {
+      if (index === size - 1) {
+        artists += item.name;
+      } else {
+        artists += item.name + '/';
+      }
+    });
+    cardTop.querySelector('.artistname').textContent = artists;
 
-      const clone = cardTop.cloneNode(true)
-      fragment.appendChild(clone)
-  })
-  contenido.appendChild(fragment)
-}
+    const clone = cardTop.cloneNode(true);
+    fragment.appendChild(clone);
+  });
+  contenido.appendChild(fragment);
+};
 
 btnBuscar.addEventListener('input', (event) => {
-  let searchedSong = event.target.value.toLowerCase()
-  console.log(searchedSong)
-  contenido.innerHTML = '' 
+  let searchedSong = event.target.value.toLowerCase();
+  console.log(searchedSong);
+  contenido.innerHTML = '';
   let filteredSongs = topTwoHundred.filter((song) => {
-    return song.trackMetadata.trackName.toLowerCase().includes(searchedSong)
-  })
-  console.log(filteredSongs)
+    return song.trackMetadata.trackName.toLowerCase().includes(searchedSong);
+  });
+  console.log(filteredSongs);
   filteredSongs.forEach((song) => {
-      cardTop.querySelector('img').setAttribute('src', song.trackMetadata.displayImageUri)
-      cardTop.querySelector('.songname').textContent = song.trackMetadata.trackName
-      let artists = ''
-      let size = song.trackMetadata.artists.length
-      song.trackMetadata.artists.forEach((item, index) => {
-        if (index === size-1){
-          artists += item.name
-        } else{
-          artists += item.name + '/'
-        }
-      })
-      cardTop.querySelector('.artistname').textContent = artists
+    cardTop.querySelector('img').setAttribute('src', song.trackMetadata.displayImageUri);
+    cardTop.querySelector('.songname').textContent = song.trackMetadata.trackName;
+    let artists = '';
+    let size = song.trackMetadata.artists.length;
+    song.trackMetadata.artists.forEach((item, index) => {
+      if (index === size - 1) {
+        artists += item.name;
+      } else {
+        artists += item.name + '/';
+      }
+    });
+    cardTop.querySelector('.artistname').textContent = artists;
 
-      const clone = cardTop.cloneNode(true)
-      fragment.appendChild(clone)
-  })
-  contenido.appendChild(fragment)
-})
+    const clone = cardTop.cloneNode(true);
+    fragment.appendChild(clone);
+  });
+  contenido.appendChild(fragment);
+});
